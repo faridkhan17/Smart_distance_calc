4511a7b84e04ca4d92b40d4bef8c5f71a857fb84
farid <farid_f06@yahoo.com>
2019-10-22 10:47:34 +0200

    test

---
+++ src/mypkg/algorithm.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+"""
+algorithm
+"""
+
+import pandas as pd
+
+
+def read_file():
+    df_student = pd.read_csv('data/students.csv', sep = '\t')
+    df_practices = pd.read_csv('data/practices.csv', sep = '\t')
+    
+    df_student.rename(columns={'id':'id_of_student', 'address':'address_of_student', 'alternativeAddress1':'alternativeAddress1_of_student', 
+    'alternativeAddress2':'alternativeAddress2_of_student', 'favoriteSpecialties':'favoriteSpecialties_of_student'}, inplace=True)
+    
+    df_practices.rename(columns={'id':'id_of_practice', 'address':'address_of_practice', 'specialties':'specialties_of_practice'}, inplace=True)
+    
+    df = pd.concat([df_student, df_practices], axis=1, sort=False)
+    
+    df['Pair'] = df['id_of_student'].str.cat(df['id_of_practice'], sep=' - ')
+    return df
+
+df = read_file()
---
+++ tests/read_file.py
@@ -0,0 +1,7 @@
+import pytest
+
+from mypkg.algorithm import read_file
+
+def reading_file_to_df():
+	assert(df.shape == (50, 11))
+
